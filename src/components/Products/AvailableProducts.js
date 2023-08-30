5f4de883a86837b323efb9cfb6ecb1aeb5de3e8a
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-08-30 12:33:23 +0200

    Style the product list

--- src/components/Products/AvailableProducts.js
+++ src/components/Products/AvailableProducts.js
@@ -1,49 +1,91 @@
 import Card from '../UI/Card';
 import ProductItem from './ProductItem/ProductItem';
 import classes from './AvailableProducts.module.css';
+import dummyImage from '../../assets/zviyo.jpg';
 
-const DUMMY_MEALS = [
+const PRODUCTS = [
   {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
+    id: 1,
+    name: 'Zviyo',
+    englishName: 'Finger Millet',
+    description:
+      "Zviyo, also known as finger millet porridge, is a staple food in Zimbabwe. It's made from finger millet flour and water, cooked to a thick consistency. It's similar to sadza but specifically uses finger millet.",
+    image: dummyImage,
+    price: 16,
   },
   {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
+    id: 2,
+    name: 'Mhunga',
+    englishName: 'Sorghum',
+    description:
+      "Mhunga is another type of porridge made from sorghum flour. It's a common traditional dish in Zimbabwe, often served as a staple food. Sorghum is a drought-resistant grain that is ground into flour to make porridge.",
+    image: dummyImage,
+    price: 17.9,
   },
   {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
+    id: 3,
+    name: 'Madora',
+    englishName: 'Mopane Worms',
+    description:
+      'Madora, also known as mopane worms, are a traditional delicacy in Zimbabwe. These are large caterpillar larvae that feed on mopane trees. They are a good source of protein and are often dried, seasoned, and enjoyed as a crunchy snack or added to stews for extra flavor.',
+    image: dummyImage,
+    price: 10,
   },
   {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
+    id: 4,
+    name: 'Nyimo',
+    englishName: 'Roundnuts',
+    description:
+      'Nyimo, also known as roundnuts, are a popular snack in Zimbabwe. These nuts are commonly roasted and enjoyed for their rich flavor. They are a source of essential nutrients and are often sold by street vendors.',
+    image: dummyImage,
+    price: 9.08,
+  },
+  {
+    id: 5,
+    name: 'Mupunga',
+    englishName: 'Unrefined Rice',
+    description:
+      "Mupunga, or unrefined rice, is a staple food in Zimbabwe. It is rice that retains its outer bran layer, providing additional nutrients and a nutty flavor. It's commonly used to make porridge or rice-based dishes.",
+    image: dummyImage,
+    price: 14.18,
+  },
+  {
+    id: 6,
+    name: 'Gorosi',
+    englishName: 'Unrefined Wheat',
+    description:
+      'Gorosi is a cereal grain that is a worldwide staple food. Described as unrefined wheat used for making traditional bread (hupfu hunobikwa chingwa).',
+    image: dummyImage,
+    price: 23.45,
+  },
+  {
+    id: 7,
+    name: 'Nzungu',
+    englishName: 'Groundnuts',
+    description:
+      'Nzungu, also known as groundnuts or peanuts, are commonly enjoyed as a snack in Zimbabwe. They are also used to make peanut butter, which is a versatile ingredient used in various dishes and sauces.',
+    image: dummyImage,
+    price: 12.5,
   },
 ];
 
 const AvailableProducts = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const ProductsList = PRODUCTS.map((prod) => (
     <ProductItem
-      key={meal.id}
-      id={meal.id}
-      name={meal.name}
-      description={meal.description}
-      price={meal.price}
+      key={prod.id}
+      id={prod.id}
+      name={prod.name}
+      englishName={prod.englishName}
+      description={prod.description}
+      image={prod.image}
+      price={prod.price}
     />
   ));
 
   return (
     <section className={classes.meals}>
       <Card>
-        <ul>{mealsList}</ul>
+        <ul>{ProductsList}</ul>
       </Card>
     </section>
   );
--- src/components/Products/ProductItem/ProductItem.js
+++ src/components/Products/ProductItem/ProductItem.js
@@ -1,34 +1,21 @@
-import { useDispatch } from 'react-redux';
-import ProductItemForm from './ProductItemForm';
+import { Link } from 'react-router-dom';
 import classes from './ProductItem.module.css';
-import { addItem } from '../../../redux/cartSlice';
 
 const ProductItem = (props) => {
-  const dispatch = useDispatch();
-
-  const price = `$${props.price.toFixed(2)}`;
-
-  const addToCartHandler = (amount) => {
-    dispatch(
-      addItem({
-        id: props.id,
-        name: props.name,
-        amount: amount,
-        price: props.price,
-      })
-    );
-  };
-
   return (
-    <li className={classes.meal}>
-      <div>
-        <h3>{props.name}</h3>
-        <div className={classes.description}>{props.description}</div>
-        <div className={classes.price}>{price}</div>
-      </div>
-      <div>
-        <ProductItemForm id={props.id} onAddToCart={addToCartHandler} />
-      </div>
+    <li>
+      <Link to={`/details/${props.id}`} className={classes.meal}>
+        <img
+          src={props.image}
+          alt="ProductImage"
+          className={classes.productImage}
+        />
+        <div className={classes.productText}>
+          <h3>{props.name}</h3>
+          <span> | </span>
+          <h3>{props.englishName}</h3>
+        </div>
+      </Link>
     </li>
   );
 };
