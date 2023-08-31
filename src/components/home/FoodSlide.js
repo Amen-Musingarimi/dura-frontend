6e8f35ee1548f4c2732dbc5e219f9341a9d74e80
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-08-31 13:07:03 +0200

    Add home page basic styling

---
+++ src/components/home/FoodSlide.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import classes from './FoodSlide.module.css';
+
+const FoodSlide = ({ foods }) => {
+  const [currentFoodIndex, setCurrentFoodIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentFoodIndex((prevIndex) => (prevIndex + 1) % foods.length);
+    }, 4000);
+
+    return () => clearInterval(interval);
+  }, [foods]);
+
+  return (
+    <div
+      className={classes.foodSlideContainer}
+      style={{
+        backgroundImage: foods[currentFoodIndex]?.image, // Use image color as background
+      }}
+    >
+      <div className={classes.foodSlide}>
+        <div className={classes.fooDetails}>
+          <h2 className={classes.foodName}>{foods[currentFoodIndex]?.name}</h2>
+          <p className={classes.foodDescription}>
+            {foods[currentFoodIndex]?.description}
+          </p>
+          <Link to="/products" className={classes.navigateLink}>
+            DISCOVER MORE
+          </Link>
+        </div>
+        <div className={classes.callToActionSticker}>
+          <div className={classes.whiteBorder}>
+            <span className={classes.from}>From</span>
+            <span className={classes.lowestPrice}>$9.00</span>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FoodSlide;
