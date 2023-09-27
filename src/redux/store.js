9da8c10a97c3e6de41f3ab130a02c0c28c9fb1d8
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-09-27 14:09:53 +0200

    Add purchase history slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import AuthenticationReducer from './authenticationSlice';
 import productsReducer from './productsSlice';
 import cartReducer from './cartSlice';
+import purchaseHistoryReducer from './transactionsSlice'
 import NavBarReducer from './navBarSlice';
 
 const store = configureStore({
@@ -10,6 +11,7 @@ const store = configureStore({
     cart: cartReducer,
     nav: NavBarReducer,
     product: productsReducer,
+    purchaseHistory: purchaseHistoryReducer,
   },
 });
 
---
+++ src/redux/transactionsSlice.js
@@ -0,0 +1,64 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { getLocalStorage } from '../components/helpers/localStorage';
+
+export const fetchPurchaseHistoryAsync = createAsyncThunk(
+    'purchaseHistory/fetchPurchaseHistory',
+    async () => {
+      const token = getLocalStorage('token');
+      const user = getLocalStorage('user');
+      const user_id = user.id
+      const response = await axios.get(`http://localhost:3000/purchase_histories/${user_id}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      return response.data;
+    }
+  );
+
+export const createPurchaseHistoryAsync = createAsyncThunk(
+    'purchaseHistory/createPurchaseHistory',
+    async (purchaseData) => {
+      const token = getLocalStorage('token');
+      const response = await axios.post('http://localhost:3000/cart_items/order', purchaseData, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      return response.data;
+    }
+  );
+
+const purchaseHistorySlice = createSlice({
+  name: 'purchaseHistory',
+  initialState: {
+    purchaseHistory: [],
+    status: 'idle',
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+    .addCase(fetchPurchaseHistoryAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchPurchaseHistoryAsync.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.purchaseHistory = action.payload;
+      })
+      .addCase(fetchPurchaseHistoryAsync.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      })
+      .addCase(createPurchaseHistoryAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(createPurchaseHistoryAsync.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.purchaseHistory = action.payload;
+      })
+      .addCase(createPurchaseHistoryAsync.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default purchaseHistorySlice.reducer;
